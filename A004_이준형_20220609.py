61ac99e986ad2e0ea030024097a6192d6de25ab5
mojo1ee <69451630+mojo1ee@users.noreply.github.com>
2022-07-09 18:49:48 +0900

    Update A004_이준형_20220609.py

--- A004_이준형_20220609.py
+++ A004_이준형_20220609.py
@@ -1,5 +1,7 @@
-#Programmers 나누어 떨어지는 숫자 배열
-
+# Programmers 나누어 떨어지는 숫자 배열
+# https://school.programmers.co.kr/learn/courses/30/lessons/12910
+    
+    
 def solution(arr, divisor):
     
     answer = []
